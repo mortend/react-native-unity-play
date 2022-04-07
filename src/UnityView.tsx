b8be373ff93b34e15270e20b5f3aedeed5213294
Morten Daniel Fornes <morten@fusetools.com>
2022-04-07 15:22:12 +0700

    add UnityView.onMessage prop

--- src/UnityView.tsx
+++ src/UnityView.tsx
@@ -1,12 +1,31 @@
 import * as React from "react"
-import type { ViewProperties } from "react-native"
+import type { EventSubscription, ViewProperties } from "react-native"
 import { Platform, ViewPropTypes } from "react-native"
 import { UnityAndroidView } from "./UnityAndroidView"
 import { UnityResponderView } from "./UnityResponderView"
+import { UnityModule } from "./UnityModule"
+import * as PropTypes from "prop-types"
 
-export class UnityView extends React.Component<ViewProperties> {
+export interface UnityViewProps extends ViewProperties {
+    onMessage?: (message: any) => void
+}
+
+export class UnityView extends React.Component<UnityViewProps> {
     static propTypes = {
-        ...ViewPropTypes
+        ...ViewPropTypes,
+        onMessage: PropTypes.func
+    }
+
+    listener?: EventSubscription
+
+    componentDidMount() {
+        if (this.props.onMessage) {
+            this.listener = UnityModule.addListener(this.props.onMessage)
+        }
+    }
+
+    componentWillUnmount() {
+        this.listener?.remove()
     }
 
     render() {
