01d5e39c2e6fd93aedab9ef9e623f2260049bfd1
Morten Daniel Fornes <morten@fusetools.com>
2022-04-20 14:36:03 +0700

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { Button, View, Text } from "react-native"
+import { Button, View } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { UnityView } from "react-native-unity-play"
+import { UnityModule, UnityView } from "react-native-unity-play"
 
 const Stack = createNativeStackNavigator()
 
@@ -20,7 +20,6 @@ export default function App() {
 const Main = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Unity Screen</Text>
       <Button
         title="Go to Unity"
         onPress={() => navigation.navigate("Unity")}
@@ -30,5 +29,42 @@ const Main = ({ navigation }) => {
 }
 
 const Unity = () => {
-  return <UnityView style={{ flex: 1 }} />
+  return (
+    <UnityView style={{ flex: 1, justifyContent: "flex-end" }} onMessage={onMessage}>
+      <View style={{ flexDirection: "row", alignContent: "space-between", justifyContent: "center" }}>
+        <Button title={"setColor"} onPress={async () => console.log(await cubeApi.setColor(randomColor()))} />
+        <Button title={"toggleRotate"} onPress={async () => console.log(await cubeApi.toggleRotate())} />
+        <Button title={"getAccount"} onPress={async () => console.log(await cubeApi.getAccount())} />
+        <Button title={"fail"} onPress={async () => console.log(await cubeApi.fail())} />
+      </View>
+    </UnityView>
+  )
+}
+
+const onMessage = data => {
+  console.log("Unity message: " + data)
+}
+
+const cubeApi = {
+  setColor(color) {
+    return UnityModule.callMethod("Cube", "setColorRN", color)
+  },
+
+  toggleRotate() {
+    return UnityModule.callMethod("Cube", "toggleRotateRN")
+  },
+
+  getAccount() {
+    return UnityModule.callMethod("Cube", "getAccountRN")
+  },
+
+  fail() {
+    return UnityModule.callMethod("Cube", "failRN")
+  },
+}
+
+const randomColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")}`
 }
