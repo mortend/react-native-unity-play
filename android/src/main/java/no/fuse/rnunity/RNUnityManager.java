7132e574ecfdf24cb55efdd12acef7f9245f95fb
Morten Daniel Fornes <morten@fusetools.com>
2022-04-21 00:05:22 +0700

    [wip]

--- android/src/main/java/no/fuse/rnunity/RNUnityManager.java
+++ android/src/main/java/no/fuse/rnunity/RNUnityManager.java
@@ -3,16 +3,20 @@ package no.fuse.rnunity;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.unity3d.player.IUnityPlayerLifecycleEvents;
 import com.unity3d.player.UnityPlayer;
 
 import javax.annotation.Nonnull;
 
-public class RNUnityManager extends SimpleViewManager<UnityPlayer> implements LifecycleEventListener, View.OnAttachStateChangeListener {
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+
+public class RNUnityManager extends SimpleViewManager<UnityView> implements LifecycleEventListener, View.OnAttachStateChangeListener, IUnityPlayerLifecycleEvents {
     public static final String REACT_CLASS = "UnityView";
 
     public RNUnityManager(ReactApplicationContext reactContext) {
@@ -27,26 +31,52 @@ public class RNUnityManager extends SimpleViewManager<UnityPlayer> implements Li
     }
 
     static UnityPlayer player;
+    static UnityView singleton;
 
     @Nonnull
     @Override
-    protected UnityPlayer createViewInstance(@Nonnull ThemedReactContext reactContext) {
+    protected UnityView createViewInstance(@Nonnull ThemedReactContext reactContext) {
         Log.d("RNUnityManager", "createViewInstance");
-        final Activity activity = reactContext.getCurrentActivity();
-        if (player == null)
-            player = new UnityPlayer(activity);
-        player.addOnAttachStateChangeListener(this);
+
+        if (singleton != null) {
+            //player.displayChanged(0, null);
+            player.requestFocus();
+            return singleton;
+        }
+
+        if (player == null) {
+            Activity activity = reactContext.getCurrentActivity();
+            player = new UnityPlayer(activity, this);
+            player.resume();
+        } else {
+
+        }
+
+        UnityView view = new UnityView(reactContext.getReactApplicationContext());
+        view.addOnAttachStateChangeListener(this);
+
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT);
+        view.addView(player, 0, layoutParams);
+
         player.windowFocusChanged(true);
         player.requestFocus();
-        player.resume();
-        return player;
+
+        singleton = view;
+        return view;
     }
 
     @Override
-    public void onDropViewInstance(UnityPlayer view) {
+    public void onDropViewInstance(UnityView view) {
         Log.d("RNUnityManager", "onDropViewInstance");
-        view.removeOnAttachStateChangeListener(this);
-        super.onDropViewInstance(view);
+        return;
+
+        //view.removeOnAttachStateChangeListener(this);
+        //view.removeView(player);
+        //view.removeAllViews();
+        //super.onDropViewInstance(view);
+
+        //player.unload();
+        //player = null;
     }
 
     @Override
@@ -72,5 +102,15 @@ public class RNUnityManager extends SimpleViewManager<UnityPlayer> implements Li
     @Override
     public void onViewDetachedFromWindow(View v) {
         Log.d("RNUnityManager", "onViewDetachedFromWindow");
+    }
+
+    @Override
+    public void onUnityPlayerUnloaded() {
+        Log.d("RNUnityManager", "onUnityPlayerUnloaded");
+    }
+
+    @Override
+    public void onUnityPlayerQuitted() {
+        Log.d("RNUnityManager", "onUnityPlayerQuitted");
     }
 }
---
+++ android/src/main/java/no/fuse/rnunity/UnityView.java
@@ -0,0 +1,36 @@
+package no.fuse.rnunity;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.widget.FrameLayout;
+
+import com.unity3d.player.UnityPlayer;
+
+public class UnityView extends FrameLayout {
+    private UnityPlayer player;
+
+    public UnityView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        super.onWindowFocusChanged(hasWindowFocus);
+        if (player != null) {
+            player.windowFocusChanged(hasWindowFocus);
+        }
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (player != null) {
+            player.configurationChanged(newConfig);
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+}
