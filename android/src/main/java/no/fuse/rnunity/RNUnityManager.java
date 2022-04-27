2a7e9ca2bb870c7959dda592d25edd4c438b3271
Morten Daniel Fornes <morten@fusetools.com>
2022-04-27 21:08:49 +0700

    android: drop redundant code

--- android/src/main/java/no/fuse/rnunity/RNUnityManager.java
+++ android/src/main/java/no/fuse/rnunity/RNUnityManager.java
@@ -24,41 +24,10 @@ import javax.annotation.Nonnull;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
-class UnityPlayer2 extends UnityPlayer {
-
-    public UnityPlayer2(Context context, IUnityPlayerLifecycleEvents iUnityPlayerLifecycleEvents) {
-        super(context, iUnityPlayerLifecycleEvents);
-        //super(context);
-    }
-
-    @Override
-    protected void removeDetachedView(View child, boolean animate) {
-        Log.d("UnityPlayer2", "removeDetachedView");
-        super.removeDetachedView(child, false);
-    }
-
-    void resetParent() {
-        try {
-            Method method = View.class.getDeclaredMethod("assignParent", new Class<?>[]{ ViewParent.class });
-            method.setAccessible(true);
-            method.invoke(this, new Object[]{ null });
-            method.setAccessible(false);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
 public class RNUnityManager extends SimpleViewManager<UnityView> implements LifecycleEventListener, View.OnAttachStateChangeListener, IUnityPlayerLifecycleEvents {
     public static final String REACT_CLASS = "UnityView";
 
-    public static UnityPlayer2 player;
+    public static UnityPlayer player;
 
     public RNUnityManager(ReactApplicationContext reactContext) {
         super();
@@ -75,16 +44,15 @@ public class RNUnityManager extends SimpleViewManager<UnityView> implements Life
     @Override
     protected UnityView createViewInstance(@Nonnull ThemedReactContext reactContext) {
         Log.d("RNUnityManager", "createViewInstance");
+        final Activity activity = reactContext.getCurrentActivity();
 
         if (player == null) {
-            Activity activity = reactContext.getCurrentActivity();
-            player = new UnityPlayer2(activity, this);
+            player = new UnityPlayer(activity, this);
         } else {
             // We must pause the player to avoid hanging on last frame
             player.pause();
 
             // Repeat pause/resume after delay to workaround another glitch
-            final Activity activity = reactContext.getCurrentActivity();
             final Handler handler = new Handler(Looper.getMainLooper());
             handler.postDelayed(new Runnable() {
                 @Override
@@ -92,7 +60,7 @@ public class RNUnityManager extends SimpleViewManager<UnityView> implements Life
                     activity.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            Log.d("RNUnityManager", "pause/resume player!");
+                            Log.d("RNUnityManager", "Restarting Unity player");
                             player.pause();
                             player.resume();
                         }
@@ -102,17 +70,9 @@ public class RNUnityManager extends SimpleViewManager<UnityView> implements Life
         }
 
         if (player.getParent() != null) {
-            Log.d("RNUnityManager", "createViewInstance1");
             ((ViewGroup) player.getParent()).removeView(player);
-            player.resetParent();
+            resetParent();
         }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Log.d("RNUnityManager", "createViewInstance2");
-            player.setZ(1f);
-        }
-
-        Log.d("RNUnityManager", "createViewInstance3");
 
         UnityView view = new UnityView(reactContext.getReactApplicationContext(), player);
         view.addOnAttachStateChangeListener(this);
@@ -133,22 +93,13 @@ public class RNUnityManager extends SimpleViewManager<UnityView> implements Life
         view.removeOnAttachStateChangeListener(this);
 
         if (player.getParent() != null) {
-            Log.d("RNUnityManager", "onDropViewInstance1");
             ((ViewGroup) player.getParent()).removeView(view);
-            player.resetParent();
+            resetParent();
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Log.d("RNUnityManager", "onDropViewInstance2");
-            player.setZ(-1f);
-        }
-
-        Log.d("RNUnityManager", "onDropViewInstance3");
         Activity activity = ((ReactApplicationContext) view.getContext()).getCurrentActivity();
         ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(1, 1);
         activity.addContentView(player, layoutParams);
-
-        Log.d("RNUnityManager", "onDropViewInstance4");
     }
 
     @Override
@@ -190,5 +141,22 @@ public class RNUnityManager extends SimpleViewManager<UnityView> implements Life
     @Override
     public void onUnityPlayerQuitted() {
         Log.d("RNUnityManager", "onUnityPlayerQuitted");
+    }
+
+    static void resetParent() {
+        try {
+            Method method = View.class.getDeclaredMethod("assignParent", new Class<?>[]{ ViewParent.class });
+            method.setAccessible(true);
+            method.invoke(player, new Object[]{ null });
+            method.setAccessible(false);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
